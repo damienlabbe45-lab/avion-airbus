885c0dc0bddb455c6ce421065c499f92c362fbe3
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-03 13:52:49 +0200

    ajouts des méthodes pour faire un avion de Airbus

--- T/src/AirplaneAirbus.java
+++ T/src/AirplaneAirbus.java
@@ -1,8 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner; 
 
 public class AirplaneAirbus {
-
+	
+	public static String InputInt(Scanner input) {
+		System.out.println("veillez mettre un identifiant");
+		while(!input.hasNextInt()) input.next();
+		return input.next();
+	}
+	
+	public static String InputPhase(Scanner input) {
+		String typeproduct ="";
+		while(!Arrays.asList("etude de faisabilite","conception","definition",
+				"construction","en service","cloture").contains(typeproduct)) {
+			typeproduct =input.next().toLowerCase();
+		}
+		return typeproduct;
+	}
+	
+	public static String InputProgramm(Scanner input) {
+		String programm = "";
+		while(!programm.matches("[A-Z][0-9]+[A-Z]?")) {
+			programm = input.next();
+		}
+		return programm;
+	}
+	public static List<String[4]> AirplaneCreate(Scanner input){
+		List<String[4]> airplane = new ArrayList<>;
+		while(!input.next().equalsIgnoreCase("oui")) {
+			
+			airplane.add(new String(InputInt(input),InputProgramm(input),InputPhase(input),input.next()));
+			
+		}
+		return  airplane;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 
 	}
 
