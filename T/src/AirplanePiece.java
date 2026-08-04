5a151984710d04198a7d6520cbe0ddbcb265ea20
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-04 08:49:14 +0200

    ajout de la méthode InputDouble

--- T/src/AirplanePiece.java
+++ T/src/AirplanePiece.java
@@ -1,11 +1,18 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class AirplanePiece {
 	
 	public static void printpiece(List<String[]> pieces) {
 		System.out.println("[nom,Catégorie,prix]");
 		System.out.println(Arrays.deepToString(pieces.toArray()));
+	}
+	
+	public static String InputDouble(Scanner input) {
+		System.out.println("veillez mettre un prix");
+		while(!input.hasNextDouble()) input.next();
+		return input.next();
 	}
 	
 	public static void main(String[] args) {
