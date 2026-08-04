4180793f0d77256fd9e604bbea84bd2c49cc70e7
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-04 08:42:58 +0200

    ajout de la méthode printpiece

--- T/src/AirplanePiece.java
+++ T/src/AirplanePiece.java
@@ -1,4 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class AirplanePiece {
+	
+	public static void printpiece(List<String[]> pieces) {
+		System.out.println("[nom,Catégorie,prix]");
+		System.out.println(Arrays.deepToString(pieces.toArray()));
+	}
+	
+	public static void main(String[] args) {
+		if( args.length > 0) throw new IllegalArgumentException(" pas d'arguments");
+	}
 
 }
